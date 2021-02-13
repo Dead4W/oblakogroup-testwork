c2d2caabc2b73d5745ec432d55dda01472e51880
Dead4W <dead4w@gmail.com>
2021-02-13 11:22:25 +0000

    origin header and new todo changed

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -20,8 +20,9 @@ class ProjectController < ActionController::API
   end
 
   def do
-  	  todo_id = params.permit(:todo_id)['todo_id'].to_i
-  	  project_id = params.permit(:project_id)['project_id'].to_i
+  	todo_id = params.permit(:todo_id)['todo_id'].to_i
+  	project_id = params.permit(:project_id)['project_id'].to_i
+    state = ActiveModel::Type::Boolean.new.cast(params.permit(:state)['state'])
 
 	  t = Todo.find(todo_id)
 
@@ -33,7 +34,7 @@ class ProjectController < ActionController::API
 	  	return render :json => {:message => "bad project id"}
 	  end 
 
-      t.isCompleted = !t.isCompleted
+      t.isCompleted = state
       t.project.id
       t.save!
 
@@ -42,30 +43,24 @@ class ProjectController < ActionController::API
 
   def new
 
-  	  if todo_params.empty?
-  	  	return render :json => {:message => "bad todo param"}
-  	  end
+  	if todo_params.empty?
+  		return render :json => {:message => "bad todo param"}
+  	end
 
-  	  if not project_params.empty?
-  	  	p = Project.find(project_params)
+  	if not new_project_params.empty?
+  	 	p = Project.find_by(new_project_params)
+    end
 
-		if p.empty?
-			return render :json => {:message => "project not finded"}
-		end
+		if p.blank?
+  	  p = Project.create(new_project_params)
 
-  	  elsif not new_project_params.empty?
-  	  	p = Project.create(new_project_params)
+  	  p.save!
+  	end
 
-  	  	p.save!
-
-        t = Todo.create(todo_params)
-        t.project = p
-        t.save!
-  	  else
-
-	    return render :json => {:message => "bad project param"}
-  	  end
-
+    t = Todo.create(todo_params)
+    t.project = p
+    t.save!
+      
 	  return render :json => {
 	  	:project => p.to_hash,
 	  	:todo => t.to_hash
@@ -79,10 +74,6 @@ class ProjectController < ActionController::API
 
     def new_project_params
       params.require(:project).permit(:title)
-    end
-
-    def project_params
-      params.require(:project).permit(:id)
     end
 
 end
